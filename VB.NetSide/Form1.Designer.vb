da40cfebf876a750409c97f18618a30ccfeeeacc
Abbas Aliakbari <sampad.abbas1370@gmail.com>
2018-08-10 23:24:55 +0430

    Implementation base class for client-server connection.
    add base class for asynchronous client connection to another in another language.

--- VB.NetSide/Form1.Designer.vb
+++ VB.NetSide/Form1.Designer.vb
@@ -22,10 +22,195 @@ Partial Class Form1
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
-		components = New System.ComponentModel.Container()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.ipAddress = New System.Windows.Forms.MaskedTextBox()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+		Me.port = New System.Windows.Forms.NumericUpDown()
+		Me.connect = New System.Windows.Forms.Button()
+		Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+		Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+		Me.recievedDataList = New System.Windows.Forms.RichTextBox()
+		Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.data = New System.Windows.Forms.NumericUpDown()
+		Me.sendData = New System.Windows.Forms.Button()
+		Me.GroupBox1.SuspendLayout()
+		CType(Me.port, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.GroupBox2.SuspendLayout()
+		Me.GroupBox5.SuspendLayout()
+		Me.GroupBox4.SuspendLayout()
+		CType(Me.data, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(6, 24)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(20, 13)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "IP:"
+		'
+		'ipAddress
+		'
+		Me.ipAddress.Location = New System.Drawing.Point(32, 21)
+		Me.ipAddress.Mask = "000.000.000.000"
+		Me.ipAddress.Name = "ipAddress"
+		Me.ipAddress.Size = New System.Drawing.Size(100, 20)
+		Me.ipAddress.TabIndex = 1
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Location = New System.Drawing.Point(-3, 49)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(29, 13)
+		Me.Label2.TabIndex = 0
+		Me.Label2.Text = "Port:"
+		'
+		'GroupBox1
+		'
+		Me.GroupBox1.Controls.Add(Me.port)
+		Me.GroupBox1.Controls.Add(Me.ipAddress)
+		Me.GroupBox1.Controls.Add(Me.connect)
+		Me.GroupBox1.Controls.Add(Me.Label1)
+		Me.GroupBox1.Controls.Add(Me.Label2)
+		Me.GroupBox1.Location = New System.Drawing.Point(6, 19)
+		Me.GroupBox1.Name = "GroupBox1"
+		Me.GroupBox1.Size = New System.Drawing.Size(143, 118)
+		Me.GroupBox1.TabIndex = 2
+		Me.GroupBox1.TabStop = False
+		Me.GroupBox1.Text = "Server Address:"
+		'
+		'port
+		'
+		Me.port.Location = New System.Drawing.Point(32, 47)
+		Me.port.Maximum = New Decimal(New Integer() {65556, 0, 0, 0})
+		Me.port.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+		Me.port.Name = "port"
+		Me.port.Size = New System.Drawing.Size(46, 20)
+		Me.port.TabIndex = 3
+		Me.port.Value = New Decimal(New Integer() {1, 0, 0, 0})
+		'
+		'connect
+		'
+		Me.connect.Location = New System.Drawing.Point(32, 73)
+		Me.connect.Name = "connect"
+		Me.connect.Size = New System.Drawing.Size(100, 23)
+		Me.connect.TabIndex = 3
+		Me.connect.Text = "Connect"
+		Me.connect.UseVisualStyleBackColor = True
+		'
+		'GroupBox2
+		'
+		Me.GroupBox2.Controls.Add(Me.GroupBox5)
+		Me.GroupBox2.Controls.Add(Me.GroupBox4)
+		Me.GroupBox2.Controls.Add(Me.GroupBox1)
+		Me.GroupBox2.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.GroupBox2.Location = New System.Drawing.Point(5, 5)
+		Me.GroupBox2.Name = "GroupBox2"
+		Me.GroupBox2.Size = New System.Drawing.Size(444, 231)
+		Me.GroupBox2.TabIndex = 4
+		Me.GroupBox2.TabStop = False
+		Me.GroupBox2.Text = "Connect To Server"
+		'
+		'GroupBox5
+		'
+		Me.GroupBox5.Controls.Add(Me.recievedDataList)
+		Me.GroupBox5.Location = New System.Drawing.Point(155, 19)
+		Me.GroupBox5.Name = "GroupBox5"
+		Me.GroupBox5.Size = New System.Drawing.Size(282, 203)
+		Me.GroupBox5.TabIndex = 1
+		Me.GroupBox5.TabStop = False
+		Me.GroupBox5.Text = "Recieved Data:"
+		'
+		'recievedDataList
+		'
+		Me.recievedDataList.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.recievedDataList.Location = New System.Drawing.Point(3, 16)
+		Me.recievedDataList.Name = "recievedDataList"
+		Me.recievedDataList.ReadOnly = True
+		Me.recievedDataList.Size = New System.Drawing.Size(276, 184)
+		Me.recievedDataList.TabIndex = 5
+		Me.recievedDataList.Text = ""
+		'
+		'GroupBox4
+		'
+		Me.GroupBox4.Controls.Add(Me.Label3)
+		Me.GroupBox4.Controls.Add(Me.data)
+		Me.GroupBox4.Controls.Add(Me.sendData)
+		Me.GroupBox4.Location = New System.Drawing.Point(8, 143)
+		Me.GroupBox4.Name = "GroupBox4"
+		Me.GroupBox4.Size = New System.Drawing.Size(141, 81)
+		Me.GroupBox4.TabIndex = 0
+		Me.GroupBox4.TabStop = False
+		Me.GroupBox4.Text = "Send Data"
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Location = New System.Drawing.Point(6, 21)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(33, 13)
+		Me.Label3.TabIndex = 4
+		Me.Label3.Text = "Data:"
+		'
+		'data
+		'
+		Me.data.Location = New System.Drawing.Point(45, 19)
+		Me.data.Maximum = New Decimal(New Integer() {-1, 0, 0, 0})
+		Me.data.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+		Me.data.Name = "data"
+		Me.data.Size = New System.Drawing.Size(64, 20)
+		Me.data.TabIndex = 3
+		Me.data.Value = New Decimal(New Integer() {1, 0, 0, 0})
+		'
+		'sendData
+		'
+		Me.sendData.Location = New System.Drawing.Point(6, 49)
+		Me.sendData.Name = "sendData"
+		Me.sendData.Size = New System.Drawing.Size(117, 23)
+		Me.sendData.TabIndex = 0
+		Me.sendData.Text = "Send"
+		Me.sendData.UseVisualStyleBackColor = True
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(800, 450)
+		Me.ClientSize = New System.Drawing.Size(454, 241)
+		Me.Controls.Add(Me.GroupBox2)
+		Me.MaximizeBox = False
+		Me.MaximumSize = New System.Drawing.Size(470, 280)
+		Me.MinimizeBox = False
+		Me.MinimumSize = New System.Drawing.Size(470, 280)
+		Me.Name = "Form1"
+		Me.Padding = New System.Windows.Forms.Padding(5)
 		Me.Text = "Form1"
+		Me.GroupBox1.ResumeLayout(False)
+		Me.GroupBox1.PerformLayout()
+		CType(Me.port, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.GroupBox2.ResumeLayout(False)
+		Me.GroupBox5.ResumeLayout(False)
+		Me.GroupBox4.ResumeLayout(False)
+		Me.GroupBox4.PerformLayout()
+		CType(Me.data, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+
 	End Sub
 
+	Friend WithEvents Label1 As Label
+	Friend WithEvents ipAddress As MaskedTextBox
+	Friend WithEvents Label2 As Label
+	Friend WithEvents GroupBox1 As GroupBox
+	Friend WithEvents port As NumericUpDown
+	Friend WithEvents connect As Button
+	Friend WithEvents GroupBox2 As GroupBox
+	Friend WithEvents GroupBox5 As GroupBox
+	Friend WithEvents recievedDataList As RichTextBox
+	Friend WithEvents GroupBox4 As GroupBox
+	Friend WithEvents Label3 As Label
+	Friend WithEvents data As NumericUpDown
+	Friend WithEvents sendData As Button
 End Class
--- VB.NetSide/Form1.vb
+++ VB.NetSide/Form1.vb
@@ -1,3 +1,179 @@
-﻿Public Class Form1
+﻿Imports System
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Threading
+Imports System.Text
 
+
+Public Class Form1
+
+	Private client As AsynchronousClient = Nothing
+	Private address As IPAddress = Nothing
+
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+	End Sub
+
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles connect.Click
+
+
+		If (ipAddress.Text IsNot "") Then
+			If (System.Net.IPAddress.TryParse(ipAddress.Text, address)) Then
+				If (IsNothing(client)) Then
+					client = New AsynchronousClient(SocketType.Stream, ProtocolType.Tcp)
+				End If
+				client.Connect(address, port.Value)
+				AddHandler client.ReceivedData, AddressOf onRecievedData
+				AddHandler client.Loged, AddressOf onLogEvent
+			End If
+		End If
+	End Sub
+
+	Private Sub onLogEvent(message As String)
+		Me.BeginInvoke(Sub() recievedDataList.Text += message)
+	End Sub
+
+	Private Sub onRecievedData(data As String)
+		Me.BeginInvoke(Sub() recievedDataList.Text += data)
+	End Sub
+
+	Private Sub SendData_Click(sender As Object, e As EventArgs) Handles sendData.Click
+		Dim dataInt As Int32 = data.Value
+		client.Send(dataInt.ToString())
+	End Sub
 End Class
+
+' State object for receiving data from remote device.  
+Public Class StateObject
+
+	' Client socket.  
+	Public workSocket As Socket = Nothing
+
+	' Size of receive buffer.  
+	Public Const BufferSize As Integer = 256
+
+	' Receive buffer.  
+	Public buffer() As Byte = New Byte((BufferSize) - 1) {}
+
+	' Received data string.  
+	Public sb As StringBuilder = New StringBuilder
+End Class
+
+Public Class AsynchronousClient
+
+	' ManualResetEvent instances signal completion.  
+	Public connectDone As ManualResetEvent = New ManualResetEvent(False)
+
+	Public sendDone As ManualResetEvent = New ManualResetEvent(False)
+
+	Public receiveDone As ManualResetEvent = New ManualResetEvent(False)
+
+
+	' The response from the remote device.  
+	Public response As String = String.Empty
+
+	Private client As Socket = Nothing
+
+	Public Event ReceivedData(data As String)
+	Public Event Loged(message As String)
+
+	Public Sub New(socketType As SocketType, protocolType As ProtocolType)
+		client = New Socket(socketType, protocolType)
+
+	End Sub
+
+	Private Sub ConnectCallback(ByVal ar As IAsyncResult)
+		Try
+			' Retrieve the socket from the state object.  
+			Dim client As Socket = CType(ar.AsyncState, Socket)
+			' Complete the connection.  
+			client.EndConnect(ar)
+			RaiseEvent Loged("Socket connected to " + client.RemoteEndPoint.ToString())
+			' Signal that the connection has been made.  
+			connectDone.Set()
+			Receive(client)
+		Catch e As Exception
+			RaiseEvent Loged(e.ToString)
+		End Try
+
+	End Sub
+
+	Private Sub Receive(ByVal client As Socket)
+		Try
+			' Create the state object.  
+			Dim state As StateObject = New StateObject
+			state.workSocket = client
+			' Begin receiving the data from the remote device.  
+			client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, New AsyncCallback(AddressOf ReceiveCallback), state)
+		Catch e As Exception
+			RaiseEvent Loged(e.ToString)
+		End Try
+
+	End Sub
+
+	Private Sub ReceiveCallback(ByVal ar As IAsyncResult)
+		Try
+			' Retrieve the state object and the client socket   
+			' from the asynchronous state object.  
+			Dim state As StateObject = CType(ar.AsyncState, StateObject)
+			Dim client As Socket = state.workSocket
+			' Read data from the remote device.  
+			Dim bytesRead As Integer = client.EndReceive(ar)
+			If (bytesRead > 0) Then
+				' There might be more data, so store the data received so far.
+				Dim str As String = Encoding.ASCII.GetString(state.buffer, 0, bytesRead)
+				state.sb.Append(str)
+				' Get the rest of the data.  
+
+				If (str.Length > 1) Then
+					RaiseEvent ReceivedData(str)
+				End If
+
+				' Signal that all bytes have been received.  
+				receiveDone.Set()
+				client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, New AsyncCallback(AddressOf ReceiveCallback), state)
+			Else
+				' All the data has arrived; put it in response.  
+				' Must change strategye to best detect packet 
+			End If
+
+		Catch e As Exception
+			RaiseEvent Loged(e.ToString)
+		End Try
+
+	End Sub
+
+	Private Sub Send(ByVal client As Socket, ByVal data As String)
+		' Convert the string data to byte data using ASCII encoding.  
+		Dim byteData() As Byte = Encoding.ASCII.GetBytes(data)
+		' Begin sending the data to the remote device.  
+		client.BeginSend(byteData, 0, byteData.Length, 0, New AsyncCallback(AddressOf SendCallback), client)
+	End Sub
+
+	Private Sub SendCallback(ByVal ar As IAsyncResult)
+		Try
+			' Retrieve the socket from the state object.  
+			Dim client As Socket = CType(ar.AsyncState, Socket)
+			' Complete sending the data to the remote device.  
+			Dim bytesSent As Integer = client.EndSend(ar)
+			RaiseEvent Loged("Sent " + bytesSent.ToString() + " bytes to server.")
+			' Signal that all bytes have been sent.  
+			sendDone.Set()
+		Catch e As Exception
+			Console.WriteLine(e.ToString)
+		End Try
+
+	End Sub
+
+	Public Sub Connect(ipAddress As IPAddress, port As Int32)
+		client.BeginConnect(ipAddress, port, New AsyncCallback(AddressOf ConnectCallback), client)
+	End Sub
+
+	Public Sub Send(ByVal data As String)
+		' Convert the string data to byte data using ASCII encoding.  
+		Dim byteData() As Byte = Encoding.ASCII.GetBytes(data & vbLf)
+		' Begin sending the data to the remote device.  
+		'client.BeginSend(byteData, 0, byteData.Length, 0, New AsyncCallback(AddressOf SendCallback), client)
+		client.Send(byteData)
+	End Sub
+End Class
+
